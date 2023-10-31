858f2ce7da05dbd8a76285a9ea15ef1341680b16
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-10-31 02:04:45 -0400

    updating dist files

--- dist/helpers/cwd.js
+++ dist/helpers/cwd.js
@@ -1,5 +1,3 @@
-"use strict";
 // ./src/helpers/cwd.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = process.env.INIT_CWD;
+export default process.env.INIT_CWD;
 //# sourceMappingURL=cwd.js.map
